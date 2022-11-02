a881f2f5435b374e9475eefcef0ec65fa95f9685
Max Denishchuk <maks.denishchuk@gmail.com>
2022-11-02 19:09:23 +0200

    //

--- src/components/Users/Users.styled.jsx
+++ src/components/Users/Users.styled.jsx
@@ -1,6 +1,5 @@
 import styled from '@emotion/styled';
 import { FormStyles } from 'components/Form/Form.styled';
-import { renderToPipeableStream } from 'react-dom/server';
 
 export const FindUser = FormStyles.withComponent('div');
 
