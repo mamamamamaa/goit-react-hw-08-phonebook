cbbaba8b9ec34e34e11761b64a207a8d68899950
mamamamamaa <maks.denishchuk@gmail.com>
2022-11-19 14:34:12 +0200

    //

--- src/components/Layout/Layout.jsx
+++ src/components/Layout/Layout.jsx
@@ -1,10 +1,12 @@
 import { Suspense } from 'react';
 import { Outlet } from 'react-router-dom';
 import { AppBar } from '../AppBar/AppBar';
+import { Toaster } from 'react-hot-toast';
 
 export const Layout = () => {
   return (
     <div>
+      <Toaster position="top-center" reverseOrder={false} />
       <AppBar />
       <div className="shadow-[inset_0px_0px_8px_0px_rgba(0,0,0,0.75)]">
         <div className="flex justify-center container mx-auto h-[90vh] py-10">
